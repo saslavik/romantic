d2d2c26425e43c01b2f77e06d5d2cbdd9d6cd369
saslavik <saslavik@gmail.com>
2021-02-04 18:00:33 +0200

    result added

---
+++ src/components/Results.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <table>
+          <tbody>
+            <tr v-for="(result, index) in results" :key="index">
+              <td>
+                <img :src="result[6][2]" :alt="result[6][1]">
+              </td>
+              <td>
+                <h3>{{ result[6][1] }}</h3>
+                <h6>Дата вылета: {{ result[0]}}</h6>
+                <h6>Кол-во дней: {{ result[3] }}</h6>
+                <h6>Дата возврата: {{ result[4] }}</h6>
+                <h6>Рацион: {{ result[7][1] }}</h6>
+                <small>Номер: {{ result[8][1] }}</small>
+              </td>
+              <td>
+                <h2>{{ result[10].total }} {{ result[10].currency }}</h2>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    results: {
+      type: Array,
+      required: true,
+    }
+  },
+  mounted() {
+    console.log(this.results);
+  }
+}
+</script>
--- src/pages/Findtour.vue
+++ src/pages/Findtour.vue
@@ -21,12 +21,16 @@
           </div>
           <div class="date">
             <AppInput type="Date" v-model="date.from">Заезд с</AppInput>
-            <AppInput type="Date" v-model="date.to">Заезд до(max. 15 дней)</AppInput>
+            <AppInput type="Date" v-model="date.to">Заезд до (max. 15 дней)</AppInput>
+          </div>
+          <div class="clients">
+            <AppInput type="number" v-model="passengers.adult">Кол-во взрослых:</AppInput>
+            <AppInput type="number" v-model="passengers.children">Кол-во детей до 12 лет:</AppInput>
           </div>
 
           <AppButton>Test</AppButton>
-
         </form>
+        <Results v-if="resData" :results='resData'></Results>
       </div>
     </section>
   </div>
@@ -37,27 +41,35 @@ import AppButton from '@/components/UI/Controls/Button.vue';
 import AppInput from '@/components/UI/Controls/Input.vue';
 import countries from '@/data/countries';
 import destinations from '@/data/destinations';
+import accommodations from '@/data/accommodations';
+import axios from 'axios';
+import Results from '@/components/Results.vue'
 
 export default {
   components: {
     AppButton,
     AppInput,
+    Results,
   },
   data() {
     return {
       departureCode: 1002131,
       destinationCode: 5732,
+      passengers: {
+        adult: "2",
+        children: "0",
+        birthday: '04.02.2020',
+        childrenSend: ''
+      },
       date: {
         from: null,
         fromSend: null,
         to: null,
         toSend: null,
       },
+      resData: null,
     }
   },
-  // mounted() {
-  //   this.$store.dispatch('getList')
-  // },
   computed: {
     localeDate() {
       this.date.fromSend = (new Date(this.date.from)).toLocaleDateString()
@@ -69,11 +81,27 @@ export default {
     },
     destinations() {
       return destinations
+    },
+    accommodations() {
+      return accommodations.find(el => el.adult === +this.passengers.adult && el.children === +this.passengers.children).accommodationId;
+    },
+    childrenSend() {
+      this.passengers.childrenSend = '';
+      for (let i = 1; i <= +this.passengers.children; i++) {
+        this.passengers.childrenSend += `&birthday${i}=${this.passengers.birthday}`
+      }
+      console.log(this.c)
     }
   },
   methods: {
     onSubmit() {
+      this.accommodations;
       this.localeDate;
+      this.childrenSend;
+      return axios.get(`https://search.tez-tour.com/tariffsearch/getResult?priceMin=0&priceMax=150000&currency=18864&nightsMin=6&nightsMax=14&hotelClassId=269506&accommodationId=${this.accommodations}${this.passengers.childrenSend}&rAndBId=15350&tourType=1&locale=ru&cityId=${this.departureCode}&countryId=${this.destinationCode}&after=${this.date.fromSend}&before=${this.date.toSend}&hotelClassBetter=true&rAndBBetter=true&hotelInStop=false&specialInStop=false&noTicketsTo=false&noTicketsFrom=false&promoFlag=true&version=2&searchTypeId=3`)
+        .then(res => {
+          this.resData = res.data.data;
+        })
       console.log(this.departureCode + this.date.fromSend + ' и до ' + this.date.toSend);
     }
   }
