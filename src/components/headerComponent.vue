6ffa10ef4a62924ec84e480b9a72fd397505c017
saslavik <saslavik@gmail.com>
2021-02-06 21:56:08 +0200

    new style

--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -17,7 +17,7 @@
           <ul class="navbar-list">
             <li class="navbar-item" v-for="link in links" :key="link.title">
               <router-link
-                class="navbar-link"
+                class="navbar-tab"
                 :to="link.url"
                 :title="link.title">
                   {{ link.title }}
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,40 @@
 <template>
-  <div class="wrapper-content wrapper-content--fixed">
-    <div class="container">
-      test
-    </div>
-  </div>
+    <carousel  :perPage='1'>
+      <slide>
+        <img src="https://cdn.pixabay.com/photo/2018/03/22/19/39/panoramic-3251636_960_720.jpg" alt="">
+        <div class="container">Slide 1 Content</div>
+
+      </slide>
+      <slide>
+        <img src="https://cdn.pixabay.com/photo/2016/03/16/16/31/ayasofia-1261148_960_720.jpg" alt="">
+
+        Slide 2 Content
+      </slide>
+      <slide>
+        Slide 3 Content
+      </slide>
+    </carousel>
+
 </template>
+
+<style lang="scss">
+
+.VueCarousel-wrapper {
+  position: relative;
+  height: 300px;
+  img {
+  opacity: 0.75;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  z-index: -99;
+  width: 100%;
+}
+}
+.VueCarousel-dot-container {
+  position: absolute;
+  bottom: 0;
+  padding: 20px;
+  margin-top: -20px !important;
+}
+</style>
