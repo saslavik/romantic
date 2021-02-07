873b25832c6c551d132b2517920afe182048ba1d
saslavik <saslavik@gmail.com>
2021-02-07 08:46:39 +0200

    delete

--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -2,7 +2,7 @@
   <div class="wrapper--content">
     <div class="container">
       <div class="top">
-        <div class="logo">ROMANTIC TUR</div>
+        <div class="logo"><h2>Romantic Tur logo</h2></div>
         <div class="call">
           <div class="phone">+(373) 684 75 097</div>
           <div class="callMe">
@@ -17,7 +17,7 @@
           <ul class="navbar-list">
             <li class="navbar-item" v-for="link in links" :key="link.title">
               <router-link
-                class="navbar-link"
+                class="tab"
                 :to="link.url"
                 :title="link.title">
                   {{ link.title }}
